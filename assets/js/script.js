27540cb3e55f30fc2404208dd4fd068ca464e8c2
gustavo <santoscarvalhol859@gmail.com>
2024-11-25 09:11:31 -0300

    upgrade

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+window.alert(`Bem vindo ao nosso site`)
+
+window.confirm(`Quer adquirir novas informações?`)
+
+// let resposta = prompt("Qual é o seu nome?");
+// if (resposta !==null) {
+//     alert("Olá , "+ resposta + "!");
+// } else {
+//     alert("Você não inseriu uma resposta.");
+// }
